f758a08e91d4532c7721a50fd102ff6c34513049
Jude Kikuyu <jkikuyu@gmail.com>
2018-04-11 17:42:57 +0300

    add Constant files and VendRequestController

---
+++ src/main/java/com/ipayafrica/elipapower/Invariable.java
@@ -0,0 +1,53 @@
+package com.ipayafrica.elipapower;
+
+public class Invariable {
+
+	public Invariable() {
+		// TODO Auto-generated constructor stub
+	}
+
+    public static final String IPAY = "ipayMsg";
+    
+    public static final String TERM = "term";
+    
+    public static final String SEQNUM = "seqNum";
+    
+    public static final String  TIME = "time";
+    
+    public static final String  ELECMSG = "elecMsg";
+    
+    public static final String VENDREQ ="vendReq";
+    
+    public static final String REF ="ref" ;
+    
+    public static final String RES ="res";
+    
+    public static final String  CUR = "amt cur";
+    
+    public static final String  NUM = "numTokens";
+    
+    public static final String METER ="meter";
+    
+    public static final String PAYTYPE ="payType" ;
+    
+    public static final String LASTREQ = "vendLastReq";
+    
+    public static final String CUSTINFO = "custInfoReq";
+    
+    public static final String  VER = "ver";
+    
+    public static final String  VENDLASTREQ = "vendLastReq";
+    
+    public static final String VENDLASTRES ="vendLastRes";
+    
+    public static final String REPCOUNT ="repcount" ;
+    
+    public static final String  ORIGTIMEE = "origTime";
+    
+    public static final String  VENDREVREQ = "vendRevReq";
+    
+    public static final String  VENDREVRES = "vendRevRes";
+
+    
+
+}
---
+++ src/main/java/com/ipayafrica/elipapower/webapp/controller/VendReqController.java
@@ -0,0 +1,12 @@
+package com.ipayafrica.elipapower.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VendReqController {
+
+	public VendReqController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
