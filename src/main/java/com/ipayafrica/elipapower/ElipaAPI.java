e7fc8a271e306dc62e3dd1b57e7b4217699733fb
Jude Kikuyu <jkikuyu@gmail.com>
2018-05-23 00:46:00 +0300

    Enable scheduler

--- src/main/java/com/ipayafrica/elipapower/ElipaAPI.java
+++ src/main/java/com/ipayafrica/elipapower/ElipaAPI.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages={"com.ipayafrica.elipapower"})// same as @Configuration @EnableAutoConfiguration @ComponentScan
-
+@EnableScheduling
 public class ElipaAPI extends SpringBootServletInitializer{
 
 	public ElipaAPI() {
